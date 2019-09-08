5ac39102618d834723d6c8edb660db2c52341d87
Yash Agarwal <yashagl1620@gmail.com>
2019-09-09 01:30:21 +0530

    Updates

---
+++ precache-manifest.4fe8b30fbd9e04353058154e6ac7965f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fbe6548f74a8d49dda4110c6e66409",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5efa5ce0d4769b2d6e83",
+    "url": "/static/css/2.3fc73323.chunk.css"
+  },
+  {
+    "revision": "d1803cafdc1abb90cbaf",
+    "url": "/static/css/main.2862d4aa.chunk.css"
+  },
+  {
+    "revision": "5efa5ce0d4769b2d6e83",
+    "url": "/static/js/2.ca50d5b8.chunk.js"
+  },
+  {
+    "revision": "d1803cafdc1abb90cbaf",
+    "url": "/static/js/main.89bb80e9.chunk.js"
+  },
+  {
+    "revision": "52123b38d11f0f357a17",
+    "url": "/static/js/runtime~main.b3935c19.js"
+  },
+  {
+    "revision": "1d7df1acb5b5a28bb50698e69a9f2119",
+    "url": "/static/media/android-logo.1d7df1ac.png"
+  },
+  {
+    "revision": "b36033e834e4e2c488622ef62c292604",
+    "url": "/static/media/ditapplogo.b36033e8.png"
+  },
+  {
+    "revision": "564655960d4fae41d6294de413c496cc",
+    "url": "/static/media/dituappstats.56465596.png"
+  },
+  {
+    "revision": "131ee3e3a7de7221ad877edfbc91a1b1",
+    "url": "/static/media/ditweb1.131ee3e3.png"
+  },
+  {
+    "revision": "5fdd32ef4dd94175fb72a2719019c0ad",
+    "url": "/static/media/html-css.5fdd32ef.png"
+  },
+  {
+    "revision": "ef05f0d6b25d96879ed6866f17c86dbe",
+    "url": "/static/media/react-logo.ef05f0d6.png"
+  },
+  {
+    "revision": "01c6377597f1888d7e88caad45186d4c",
+    "url": "/static/media/yash1.01c63775.png"
+  },
+  {
+    "revision": "68d3161c5788cf79f4ec819c7b4c34fe",
+    "url": "/static/media/yash_agarwal_resume.68d3161c.pdf"
+  },
+  {
+    "revision": "31f87dac42bc740db50fc6a964495534",
+    "url": "/static/media/youthopia.31f87dac.png"
+  }
+]);
